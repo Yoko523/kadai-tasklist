66dfd8e378abda858dabd7722b87193c3ce1e44d
Yoko.Okamoto <ec2-user@ip-172-31-40-144.us-east-2.compute.internal>
2018-04-13 13:02:40 +0000

    login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,9 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
   
   def index
-    @tasks = Task.all
+     @tasks = current_user.tasks.order('created_at DESC')
   end
   
   def show
@@ -13,8 +14,8 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
-    
+    @task = current_user.tasks.build(task_params)
+  
     if @task.save
       flash[:success] = 'Taskが正常に投稿されました'
       redirect_to @task
@@ -54,4 +55,5 @@ end
 def task_params
   params.require(:task).permit(:content, :status)
 end
-  end
+end
+
---
+++ app/controllers/toppages_controller.rb
@@ -0,0 +1,8 @@
+class ToppagesController < ApplicationController
+  def index
+    if logged_in?
+      @user = current_user
+      @task = current_user.tasks.build
+    end
+ end
+end
